bdceb41d89bfb3caa2b76efdb87660f04e390b3c
Yashar <yashar@bravenew.world>
2018-01-11 18:38:50 -0400

    added express

---
+++ server.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+
+
+app.use(express.static('public'));
+
+// start Express on port 8080
+app.listen(8080, () => {
+    console.log('Server Started on http://localhost:8080');
+    console.log('Press CTRL + C to stop server');
+});
